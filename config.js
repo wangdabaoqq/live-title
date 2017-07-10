807da7f1f23d5ab2dd17093f81a2b3e46f9a209e
wangdabao <614692339@qq.com>
2017-07-10 16:43:32 +0800

    update

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	title: '三轮车',
+	wording: {
+		noAccess: '抱歉，你没有权限访问'
+	},
+	albums: {
+		"Me": {
+		  description : "看似很自然",
+		  thumbnail : "QQ图片20170615143959.jpg",
+		  sort: 1
+		}, 
+		"Dog": {
+		  description : "pipiu",
+		  thumbnail : "11.jpg",
+		  name: "黑"
+		},
+		"High-Students": {
+		  description : "还是哪天",
+		  thumbnail : "Ah括弧(4).jpg",
+		  name: "我说是的"
+		},
+		"Sign-students": {
+		  description : "祝天下所有的情侣都是失散多年的兄妹",
+		  thumbnail : "1.jpg",
+		  name: "我说是的"
+		},
+		"Ta" : {
+		"description" : "田yy同学你好,我是王某某....",     // 该相册的描述；如果没有，则不展示
+		"thumbnail" : "QQ图片20170615143943.png",             // 封面图；如果没有，则默认取第一张作为封面
+		"name": "第七章",                          // 相册名；如果没有，则相册名为xxx
+		"password": "tyy",						  // 私密相册，密码为233
+		"noDate": false,                          // 不展示时间；如果为true，则不展示照片时间信息；默认没有，即false
+		"sort": 1								  // 排序；为1时是时间逆序；默认或不填是时间正序
+	}
+		
+	}
+}
